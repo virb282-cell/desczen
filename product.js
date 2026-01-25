39c8031ceb3e01f0a22258588c41abcacf48df53
neurosoft <virb282@gmail.com>
2026-01-25 16:45:51 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,25 @@
+// c:/Users/virub/smartshop/product.js
+
+const productData = {
+  name: "Premium Stainless Steel 360° Rotating Laptop Stand",
+  price: 199, // INR
+  images: [
+    "ls.jpg",
+    "ls.jpg",
+    "ls.jpg"
+  ],
+  shortDescription:
+    "Upgrade your workspace with this premium stainless-steel laptop stand designed for comfort, stability, and flexibility. With 360° rotation, adjustable height, and a foldable portable design, it’s perfect for office, home, and travel use..",
+  fullDescription:
+    "This 360-degree rotating stainless steel laptop stand is designed for professionals, students, and creators who want comfort and efficiency in their daily work. Its heavy-duty stainless steel construction ensures long-lasting durability, while the multi-angle adjustable arms allow you to customize height and viewing angle easily. Whether you’re working, attending online meetings, coding, or watching videos, this stand helps maintain a healthy posture.The smooth rotation base makes it perfect for shared workspaces, presentations, and multitasking setups. With anti-slip silicone pads, your laptop stays safe and scratch-free on any surface.",
+  features: [
+    "Rotate your laptop effortlessly for presentations, collaboration, or easy screen sharing.",
+    "Made from high-quality stainless steel for superior strength, durability, and a premium look.",
+    "Raises your laptop to eye level, helping reduce neck, back, and shoulder strain.",
+    "Compact, foldable structure makes it easy to carry in a bag or store when not in use",
+    "Non-slip silicone grips on arms and base prevent scratches and keep your laptop secure.",
+    "Supports laptops up to 15.6 / 17 inches (adjust as per your spec) without wobbling."
+  ],
+  
+  paymentLink: "https://rzp.io/rzp/0adzlFnn"
+};
